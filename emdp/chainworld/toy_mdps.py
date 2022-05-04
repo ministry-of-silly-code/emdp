a7358ff2ecade6e191d2d7f02f1fbcd9a6886c40
manuel <manuel.delverme@gmail.com>
2022-05-04 18:27:40 -0400

    adapting transition matrix axes to emdp order

--- emdp/chainworld/toy_mdps.py
+++ emdp/chainworld/toy_mdps.py
@@ -9,9 +9,17 @@ def dadashi_fig2d():
 
     Returns:
         tuple (P, R, gamma) where the first element is a tensor of shape
-        (A x S x S), the second element 'R' has shape (S x A) and the
+        (S x A x S), the second element 'R' has shape (S x A) and the
         last element is the scalar (float) discount factor.
     """
-    P = np.array([[[0.7, 0.3], [0.2, 0.8]], [[0.99, 0.01], [0.99, 0.01]]])
-    R = np.array(([[-0.45, -0.1], [0.5, 0.5]]))
+    P = np.array([
+        [[0.7, 0.3],
+         [0.2, 0.8]],
+
+        [[0.99, 0.01],
+         [0.99, 0.01]]
+    ]).swapaxes(0, 1) # (A x S x T) -> (S x A x T)
+    R = np.array((
+        [[-0.45, -0.1], [0.5, 0.5]]
+    ))
     return emdp.MDP(P, R, 0.9, initial_state=np.ones(2) / 2, terminal_matrix=np.zeros((2, 2)))
